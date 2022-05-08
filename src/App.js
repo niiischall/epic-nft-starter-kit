9a6c03ae20bf57aaac59da6adc885e0c7eec19f3
geekyNischal <mailfornischal@gmail.com>
2022-05-09 02:44:12 +0530

    Linter Update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { ethers } from "ethers";
 
-import EpicNFTs from './utils/EpicNFTs.json';
+import EpicNFTs from "./utils/EpicNFTs.json";
 import "./styles/App.css";
 
 const App = () => {
@@ -40,7 +40,7 @@ const App = () => {
       });
       console.log("Connected", accounts[0]);
       setCurrentAccount(accounts[0]);
-      setupEventListener() 
+      setupEventListener();
     } catch (error) {
       console.log(error);
     }
@@ -86,20 +86,26 @@ const App = () => {
         // Same stuff again
         const provider = new ethers.providers.Web3Provider(ethereum);
         const signer = provider.getSigner();
-        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, EpicNFTs.abi, signer);
+        const connectedContract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          EpicNFTs.abi,
+          signer
+        );
 
         connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
-          console.log(from, tokenId.toNumber())
-          alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`)
+          console.log(from, tokenId.toNumber());
+          alert(
+            `Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`
+          );
         });
-        console.log("Setup event listener!")
+        console.log("Setup event listener!");
       } else {
         console.log("Ethereum object doesn't exist!");
       }
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   //Render Method.
   const renderNotConnectedContainer = () => (
@@ -126,7 +132,10 @@ const App = () => {
           {currentAccount === "" ? (
             renderNotConnectedContainer()
           ) : (
-            <button onClick={mintNft} className="cta-button connect-wallet-button">
+            <button
+              onClick={mintNft}
+              className="cta-button connect-wallet-button"
+            >
               Mint NFT
             </button>
           )}
